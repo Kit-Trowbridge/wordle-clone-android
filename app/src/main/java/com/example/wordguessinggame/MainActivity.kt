5a0f91dc9902ca2d67527d5a1b6ece6142f1c43f
kit-trowbridge <kittrow.dev@gmail.com>
2026-07-15 15:56:39 +0100

    project setup

---
+++ app/src/main/java/com/example/wordguessinggame/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.wordguessinggame
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.wordguessinggame.ui.theme.WordGuessingGameTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WordGuessingGameTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    App(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun App(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WordGuessingGame() {
+    WordGuessingGameTheme {
+        App()
+    }
+}
